b3fe22679313c957ffc60271fad04ec79dedeb1f
Derek Mackey <mackey838@gmail.com>
2023-10-20 16:43:34 -0400

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import routes from "./routes/routes.js";
-import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5000",
-      "https://bringer-assessment.onrender.com/",
-    ],
-  })
-);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 app.use("/api", routes);
 
