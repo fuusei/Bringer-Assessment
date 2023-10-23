cebab20f493267661a8125b3de7fd325e5540d03
Derek Mackey <mackey838@gmail.com>
2023-10-23 19:11:33 -0400

    delete unused assets

--- frontend/src/Timeline.js
+++ frontend/src/Timeline.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Timeline as MUITimeline } from "@mui/lab";
 import Event from "./Event";
 import CopyButton from "./CopyButton";
 
 function Timeline({ history }) {
-  console.log(history);
   const { label, status, parcel_tracking_items } = history;
   const { tracking_number, external_tracking_number } = label;
 
